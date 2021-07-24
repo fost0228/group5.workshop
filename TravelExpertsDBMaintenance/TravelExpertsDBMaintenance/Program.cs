9f13ae8218ed7a0f34a609ba8767181dd02643b7
jiang hou <80292328+fost0228@users.noreply.github.com>
2021-07-24 12:52:46 -0600

    jiang, modify code for start login
    Signed-off-by: jiang hou <80292328+fost0228@users.noreply.github.com>

--- TravelExpertsDBMaintenance/TravelExpertsDBMaintenance/Program.cs
+++ TravelExpertsDBMaintenance/TravelExpertsDBMaintenance/Program.cs
@@ -17,7 +17,9 @@ namespace TravelExpertsDBMaintenance
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmLogin());
+            
+
         }
     }
 }
