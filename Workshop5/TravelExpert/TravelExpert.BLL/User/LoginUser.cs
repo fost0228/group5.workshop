1e73d0ecae326c9197cbda1a58dee314707ff5a5
jiang hou <80292328+fost0228@users.noreply.github.com>
2021-08-08 17:49:28 -0600

    workshop 5 jianghou

---
+++ Workshop5/TravelExpert/TravelExpert.BLL/User/LoginUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelExpert.BLL
+{
+    public class LoginUser
+    {
+        [Required(ErrorMessage = "Please input userID")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please input Password")]
+        public String Password { get; set; }
+    }
+}
---
+++ Workshop5/TravelExpert/TravelExpert.GUI/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelExpert.BLL;
+
+namespace TravelExpert.GUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// login
+        /// </summary>
+        /// 
+        [HttpPost]
+        public ActionResult Login(LoginUser user)
+        {
+            return View();
+        }
+    }
+}
